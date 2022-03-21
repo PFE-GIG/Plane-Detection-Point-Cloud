d5ca933dd6e04947fab2b7e55fc0ab2a5368b3cd
corentinlange <corentin.lange@gmail.com>
2022-03-21 18:08:58 +0100

    update: generate_my_pcd.sh more computing

--- figures/generate_my_pcd.sh
+++ figures/generate_my_pcd.sh
@@ -7,4 +7,6 @@ mkdir -p $1
 ../build/pdpcComputeMultiScaleFeatures -v -i "../data/my_pcd/"$1".ply" -o $1"/"$1
 ../build/pdpcSegmentation -v -i "../data/my_pcd/"$1".ply" -s $1"/"$1"_scales.txt" -f $1"/"$1"_features.txt" -o $1"/"$1
 
-../build/pdpcPostProcess -i "../data/my_pcd/"$1".ply" -s $1"/"$1"_seg.txt" -c $1"/"$1"_comp.txt" -o $1"/"$1 -col -v -range 20 24 25 30 40 42
+../build/pdpcPostProcess -i "../data/my_pcd/"$1".ply" -s $1"/"$1"_seg.txt" -c $1"/"$1"_comp.txt" -o $1"/"$1"_range" -col v -range 0 9 10 19 20 29 30 39 40 49
+../build/pdpcPostProcess -i "../data/my_pcd/"$1".ply" -s $1"/"$1"_seg.txt" -c $1"/"$1"_comp.txt" -o $1"/"$1"_pers" -col -v -pers 10 15 20 25 30 35 40
+../build/pdpcPostProcess -i "../data/my_pcd/"$1".ply" -s $1"/"$1"_seg.txt" -c $1"/"$1"_comp.txt" -o $1"/"$1"_col" -col -v -scales 5 10 15 20 25 30 35
